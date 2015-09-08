5e8822ee9eed5ded466dd26e635aaecafb6a8e72
Joseph Lozano <lozanojoseph@gmail.com>
2015-09-08 11:35:23 -0700

    static pages controller

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,13 +1,9 @@
 class MoviesController < ApplicationController
-    
+
 
   def index
     @movies = Movie.all
     respond_to do |format|
-
-      # If you don't pass anything to render
-      format.html  # don't do anything different here
-
       format.json { render :json => @movies, :status => 201 }
     end
 
@@ -15,6 +11,9 @@ class MoviesController < ApplicationController
 
   def create
     @movie = Movie.new
+  end
+
+  def test
 
   end
 
---
+++ app/controllers/static_pages_controller.rb
@@ -0,0 +1,7 @@
+class StaticPagesController < ApplicationController
+  def index
+    @movies = Movie.all
+    @reviews = Review.all
+
+  end
+end
